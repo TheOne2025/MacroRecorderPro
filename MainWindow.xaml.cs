0554eaff96de7b6d4a90f530eaea543a7753a22e
TheOne2025 <tahabouiberidenjobs@gmail.com>
2025-06-17 18:47:18 +0200

    V1

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace MacroRecorderReplica
+{
+    public partial class MainWindow : Window
+    {
+        private Timer timer;
+        private int seconds = 0;
+        private int events = 0;
+        private double size = 0;
+        private bool isRecording = false;
+        private bool isPaused = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer = new Timer(1000);
+            timer.Elapsed += Timer_Elapsed;
+        }
+
+        private void StartRecording_Click(object sender, RoutedEventArgs e)
+        {
+            if (!isRecording)
+            {
+                isRecording = true;
+                isPaused = false;
+                seconds = 0;
+                events = 0;
+                size = 0;
+                StatusText.Text = "Grabando...";
+                StatusIndicator.Fill = Brushes.Red;
+                timer.Start();
+                RecordButton.Content = "Detener";
+                PauseButton.IsEnabled = true;
+            }
+            else
+            {
+                StopRecording();
+            }
+        }
+
+        private void PauseRecording_Click(object sender, RoutedEventArgs e)
+        {
+            isPaused = !isPaused;
+            PauseButton.Content = isPaused ? "Reanudar" : "Pausar";
+            StatusText.Text = isPaused ? "Pausado" : "Grabando...";
+        }
+
+        private void StopRecording()
+        {
+            isRecording = false;
+            isPaused = false;
+            timer.Stop();
+            RecordButton.Content = "Iniciar Grabación";
+            PauseButton.Content = "Pausar";
+            PauseButton.IsEnabled = false;
+            StatusText.Text = "Grabación detenida.";
+            StatusIndicator.Fill = Brushes.Green;
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            if (!isPaused)
+            {
+                seconds++;
+                if (new Random().NextDouble() > 0.5)
+                {
+                    events += new Random().Next(1, 5);
+                    size += new Random().NextDouble() * 0.5;
+                }
+
+                Dispatcher.Invoke(() =>
+                {
+                    TimeDisplay.Text = TimeSpan.FromSeconds(seconds).ToString(@"hh\:mm\:ss");
+                    EventDisplay.Text = events.ToString();
+                    SizeDisplay.Text = $"{size:F1} KB";
+                });
+            }
+        }
+    }
+}
